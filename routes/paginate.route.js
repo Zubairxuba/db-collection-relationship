cc499ecbe39ae9ac29f55c10717b3ae62b0e29cb
Zubairxuba <zubairakhtar721@gmial.com>
2023-04-26 12:55:29 +0500

    deleted pagination route

--- routes/paginate.route.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const Pagination = require("../controllder/paginate.controller");
-
-const PaginateRoute = Router();
-
-PaginateRoute.post("/paginate", Pagination);
-
-module.exports = PaginateRoute;
